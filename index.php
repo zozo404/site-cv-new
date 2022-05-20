ac02108466fd6f6ea8189774ba4a01bd4e1c8df8
zozo404 <enzocuny66@gmail.com>
2022-05-20 18:46:33 +0200

    rename

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require 'mail.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>zozoy.fr</title>
-    <link rel="icon" type="image/png" sizes="16x16" href="imgs/imgMassEffectA.jpg">
+    <link rel="icon" type="image/png" sizes="16x16" href="./imgs/imgMassEffectA.jpg">
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/w3.css">
     <link rel="stylesheet" href="css/hamburgers.css">

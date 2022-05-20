0b8beff0206829e4f6e5fbf3188fb7e79a0b8b3b
zozo404 <enzocuny66@gmail.com>
2022-05-21 00:17:20 +0200

    test js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,19 @@
 document.addEventListener("DOMContentLoaded",function(){
   $(function (){
 
-    $(".navbar a, footer a, .div-up a, #navbar-tel a, .presentation-div-btn-all a").on("click", function(event){
-
-        event.preventDefault();
-        var hash = this.hash;
-
-
-        
-        $('body,html').animate({scrollTop: $(hash).offset().top} , 900, function (){window.location.hash = hash;})
-
-    });
-    
+    // $(".navbar a, footer a, .div-up a, #navbar-tel a, .presentation-div-btn-all a").on("click", function(event){
+    //     event.preventDefault();
+    //     var hash = this.hash;
+    //     $('body,html').animate({scrollTop: $(hash).offset().top} , 900, function (){window.location.hash = hash;})
+    // });
+    $(document).ready(function() {
+      var hashId = window.location.hash;
+      var sectionid = $(hashId);
+      if(sectionid.length != 0) {
+        {
+          $('html,body').animate({scrollTop: $(hashId).offset().top - 180}, 500);
+        }
+      }});
   })
 
 

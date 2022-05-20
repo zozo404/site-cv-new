da9887843ec373dbe2ace71eeb128630657166c4
zozo404 <enzocuny66@gmail.com>
2022-05-20 18:33:34 +0200

    :bug: |name

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require 'mail.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>zozoy.frV2</title>
+    <title>zozoy.fr</title>
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/w3.css">
     <link rel="stylesheet" href="css/hamburgers.css">

e6a2ddbee0a86803d67c71042685f347698a118f
zozo404 <enzocuny66@gmail.com>
2022-05-20 23:54:48 +0200

    2TEST JS

--- js/app.js
+++ js/app.js
@@ -1,22 +1,5 @@
 document.addEventListener("DOMContentLoaded",function(){
-  // $(function (){
-
-  //   $(".navbar a, footer a, .div-up a, #navbar-tel a, .presentation-div-btn-all a").on("click", function(event){
-
-  //       event.preventDefault();
-  //       var hash = this.hash;
-
-
-  //       if(hash.length){
-  //         $('body,html').animate({scrollTop: $(hash).offset().top} , 900, function (){window.location.hash = hash;})
-  //       }
-
-  //   });
-    
-  // })
-
-
- 
+  
   $(document).ready(function(){
     //animation pour hamburger
     $(".hamburger").click(function() {
@@ -30,7 +13,21 @@ document.addEventListener("DOMContentLoaded",function(){
     })
   })
   
+  $(function (){
 
+    $(".navbar a, footer a, .div-up a, #navbar-tel a, .presentation-div-btn-all a").on("click", function(event){
+
+        event.preventDefault();
+        var hash = this.hash;
+
+
+        if(hash.length){
+          $('body,html').animate({scrollTop: $(hash).offset().top} , 900, function (){window.location.hash = hash;})
+        }
+
+    });
+    
+  })
      
 
 })

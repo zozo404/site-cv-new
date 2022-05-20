a0ab490851f5e0eea57124c5c359e5cddb51ecb5
zozo404 <enzocuny66@gmail.com>
2022-05-21 00:11:48 +0200

    :rocket: | name zozoy

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require 'mail.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>zozoy.fr</title>
+    <title>zozoy</title>
     <link rel="icon" type="image/png" sizes="16x16" href="./imgs/imgMassEffectA.jpg">
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/w3.css">

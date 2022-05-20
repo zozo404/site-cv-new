fea405b6990043ab8bbd10d3b77fd70b7cbe4cde
zozo404 <enzocuny66@gmail.com>
2022-05-21 00:27:17 +0200

    test js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,15 @@
 document.addEventListener("DOMContentLoaded",function(){
   $(function (){
 
-    // $(".navbar a, footer a, .div-up a, #navbar-tel a, .presentation-div-btn-all a").on("click", function(event){
-    //     event.preventDefault();
-    //     var hash = this.hash;
-    //     $('body,html').animate({scrollTop: $(hash).offset().top} , 900, function (){window.location.hash = hash;})
-    // });
+    
+    
     $(document).ready(function() {
-      // var hashId = window.location.hash;
       $(".navbar a, footer a, .div-up a, #navbar-tel a, .presentation-div-btn-all a").on("click", function(event){
-          var sectionid = $(hashId);
-          if(sectionid.length != 0) {
-            {
-              $('html,body').animate({scrollTop: window.location.hash.offset().top}, 900, function(){window.location.hash = hash});
-            }
-          }
+        event.preventDefault();
+        var hash = this.hash;
+        $('body,html').animate({scrollTop: $(hash).offset().top} , 900, function (){window.location.hash = hash;})
       });
-    });
+     });
   })
 
 
